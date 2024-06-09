8dbe5a60c08f7711788ceebf818c9a95fbb1d2ea
Tirth-Chaudhari <tirthchaudhary8080@gmail.com>
2024-06-09 10:57:38 +0530

    done

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
-import { GlobalProvider } from './context/globalcontext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <GlobalProvider>
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  </GlobalProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
